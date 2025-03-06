b2872d169e092104e9d524c63149549c13c642b2
Oleksandr Trubnikov <o.trubnikov@newlinetechnologies.net>
2025-03-06 13:56:00 +0200

    SDK UP -> 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.galleryapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.galleryapp"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
